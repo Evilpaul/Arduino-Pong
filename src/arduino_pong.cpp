2fe2115d3b77c3f50da568350cec785f885d044f
Paul Young <p.young79@outlook.com>
2016-12-03 21:33:28 +0000

    Use and enum typedefs for ball directions instead of chars

--- src/arduino_pong.cpp
+++ src/arduino_pong.cpp
@@ -22,6 +22,18 @@ const int resolution[2] = { 128, 64 };
 /******************************************************************************/
 /*--------------------------------Enumerations--------------------------------*/
 /******************************************************************************/
+typedef enum
+{
+	HzDir_Left,
+	HzDir_Right
+} HzDir;
+
+typedef enum
+{
+	VtDir_Up,
+	VtDir_Straight,
+	VtDir_Down
+} VtDir;
 
 /******************************************************************************/
 /*-----------------------------Data Structures--------------------------------*/
@@ -35,8 +47,8 @@ typedef struct
 typedef struct
 {
 	int position[2] = {20, (resolution[IDX_Y] / 2)};
-	char dirHori = 'R';
-	char dirVert = 'S';
+	HzDir dirHori = HzDir_Right;
+	VtDir dirVert = VtDir_Straight;
 } ballData;
 
 /******************************************************************************/
@@ -85,26 +97,26 @@ void loop() {
 		}
 	}
 	else {
-		if (ball.dirVert == 'U') {
+		if (ball.dirVert == VtDir_Up) {
 			// move ball up diagonally
 			ball.position[IDX_Y] = ball.position[IDX_Y] - SPEED;
 		}
 
-		if (ball.dirVert == 'D') {
+		if (ball.dirVert == VtDir_Down) {
 			// move ball down diagonally
 			ball.position[IDX_Y] = ball.position[IDX_Y] + SPEED;
 		}
 
 		if (ball.position[IDX_Y] <= 0) {
 			// bounce the ball off the top
-			ball.dirVert = 'D';
+			ball.dirVert = VtDir_Down;
 		}
 		if (ball.position[IDX_Y] >= resolution[IDX_Y]) {
 			// bounce the ball off the bottom
-			ball.dirVert = 'U';
+			ball.dirVert = VtDir_Up;
 		}
 
-		if (ball.dirHori == 'R') {
+		if (ball.dirHori == HzDir_Right) {
 			ball.position[IDX_X] = ball.position[IDX_X] + SPEED; // move ball
 			if (ball.position[IDX_X] >= (resolution[IDX_X] - 6)) {
 				// ball is at the AI edge of the screen
@@ -112,27 +124,27 @@ void loop() {
 					// ball hits AI paddle
 					if (ball.position[IDX_Y] > (ai.position + 4)) {
 						// deflect ball down
-						ball.dirVert = 'D';
+						ball.dirVert = VtDir_Down;
 					} else if (ball.position[IDX_Y] < (ai.position - 4)) {
 						// deflect ball up
-						ball.dirVert = 'U';
+						ball.dirVert = VtDir_Up;
 					} else {
 						// deflect ball straight
-						ball.dirVert = 'S';
+						ball.dirVert = VtDir_Straight;
 					}
 					// change ball direction
-					ball.dirHori = 'L';
+					ball.dirHori = HzDir_Left;
 				} else {
 					// GOAL!
 					ball.position[IDX_X] = 6; // move ball to other side of screen
-					ball.dirVert = 'S'; // reset ball to straight travel
+					ball.dirVert = VtDir_Straight; // reset ball to straight travel
 					ball.position[IDX_Y] = resolution[IDX_Y] / 2; // move ball to middle of screen
 					++player.score; // increase player score
 				}
 			}
 		}
 
-		if (ball.dirHori == 'L') {
+		if (ball.dirHori == HzDir_Left) {
 			ball.position[IDX_X] = ball.position[IDX_X] - SPEED; // move ball
 			if (ball.position[IDX_X] <= 6) {
 				// ball is at the player edge of the screen
@@ -141,19 +153,19 @@ void loop() {
 					// ball hits player paddle
 					if (ball.position[IDX_Y] > (player.position + 4)) {
 						// deflect ball down
-						ball.dirVert = 'D';
+						ball.dirVert = VtDir_Down;
 					} else if (ball.position[IDX_Y] < (player.position - 4)) {
 						// deflect ball up
-						ball.dirVert = 'U';
+						ball.dirVert = VtDir_Up;
 					} else {
 						// deflect ball straight
-						ball.dirVert = 'S';
+						ball.dirVert = VtDir_Straight;
 					}
 					// change ball direction
-					ball.dirHori = 'R';
+					ball.dirHori = HzDir_Right;
 				} else {
 					ball.position[IDX_X] = resolution[IDX_X] - 6; // move ball to other side of screen
-					ball.dirVert = 'S'; // reset ball to straight travel
+					ball.dirVert = VtDir_Straight; // reset ball to straight travel
 					ball.position[IDX_Y] = resolution[IDX_Y] / 2; // move ball to middle of screen
 					++ai.score; // increase AI score
 				}
